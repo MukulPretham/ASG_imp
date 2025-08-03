8840a58caab8191775e407e5bd5a56e0f445148d
Mukul Pretham <mukulpretham@gmail.com>
2025-08-03 14:23:34 +0530

    added a new endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express from "express"
 export const app = express()
 
 app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.get("/cpu",(req,res)=>{
     for(let i  = 0 ; i < 10000000000; i++){
         Math.random()
     }
